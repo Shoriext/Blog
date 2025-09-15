87a6ee3fc9ecfa54c2a9a2a5e92b4dfc955b5b7e
Samadar1 <urapol18@gmail.com>
2025-09-15 15:49:15 +0300

    add: PostResponse

--- src/main/java/com/shoriext/blog/dto/PostResponse.java
+++ src/main/java/com/shoriext/blog/dto/PostResponse.java
@@ -1,5 +1,30 @@
 package com.shoriext.blog.dto;
 
-public class PostResponse {
+import java.time.LocalDateTime;
 
+import lombok.Data;
+
+@Data
+public class PostResponse {
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public PostResponse(
+            Long id,
+            String title,
+            String content,
+            String author,
+            LocalDateTime createdAt,
+            LocalDateTime updatedAt) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.author = author;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
 }
