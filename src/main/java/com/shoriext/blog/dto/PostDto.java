b8b1f05c1b1331457c955886ac7d26106077a0cb
Samadar1 <urapol18@gmail.com>
2025-09-15 15:41:10 +0300

    add: PostDto

--- src/main/java/com/shoriext/blog/dto/PostDto.java
+++ src/main/java/com/shoriext/blog/dto/PostDto.java
@@ -1,5 +1,20 @@
 package com.shoriext.blog.dto;
 
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
 public class PostDto {
 
+    @NotBlank(message = "Title is mandatory")
+    @Size(min = 2, max = 100, message = "Title must be between 2 and 100 characters")
+    private String title;
+
+    @NotBlank(message = "Content cannot be empty")
+    @Size(min = 10, message = "Content must be at least 10 characters long")
+    private String content;
+
+    private String author;
 }
