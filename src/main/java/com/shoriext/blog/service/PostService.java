dde19315dcf2ad4ba81e291fd70c432c124d1e17
Samadar1 <urapol18@gmail.com>
2025-09-15 15:33:24 +0300

    add: PostService & PostServiceImpl

--- src/main/java/com/shoriext/blog/service/PostService.java
+++ src/main/java/com/shoriext/blog/service/PostService.java
@@ -1,5 +1,21 @@
 package com.shoriext.blog.service;
 
-public class PostService {
+import java.util.List;
+import java.util.Optional;
 
+import com.shoriext.blog.model.Post;
+
+public interface PostService {
+
+    List<Post> getAllPosts();
+
+    List<Post> getPostsByAuthor(String author);
+
+    Optional<Post> getPostById(Long id);
+
+    Post createPost(Post post);
+
+    Post updatePost(Long id, Post postDetails);
+
+    void deletePost(Long id);
 }
---
+++ src/main/java/com/shoriext/blog/service/PostServiceImpl.java
@@ -0,0 +1,59 @@
+package com.shoriext.blog.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.shoriext.blog.model.Post;
+import com.shoriext.blog.repository.PostRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class PostServiceImpl implements PostService {
+
+    private final PostRepository postRepository;
+
+    @Override
+    public List<Post> getAllPosts() {
+        return postRepository.findAllByOrderByCreatedAtDesc();
+    }
+
+    @Override
+    public List<Post> getPostsByAuthor(String author) {
+        return postRepository.findByAuthorOrderByCreatedAtDesc(author);
+    }
+
+    @Override
+    public Optional<Post> getPostById(Long id) {
+        return postRepository.findById(id);
+    }
+
+    @Override
+    public Post createPost(Post post) {
+        return postRepository.save(post);
+    }
+
+    @Override
+    public Post updatePost(Long id, Post postDetails) {
+        Post post = postRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Post not found with id: " + id));
+
+        post.setTitel(postDetails.getTitel());
+        post.setContent(postDetails.getContent());
+        post.setAuthor(postDetails.getAuthor());
+
+        return postRepository.save(post);
+    }
+
+    @Override
+    public void deletePost(Long id) {
+        Post post = postRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Post not found with id: " + id));
+
+        postRepository.delete(post);
+    }
+
+}
