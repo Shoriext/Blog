906d4b6bb1cdbe4ec1653b08377e07c56088105e
Samadar1 <urapol18@gmail.com>
2025-09-15 15:09:47 +0300

    add: PostRepository & CRUD-method

--- src/main/java/com/shoriext/blog/repository/PostRepository.java
+++ src/main/java/com/shoriext/blog/repository/PostRepository.java
@@ -1,5 +1,18 @@
 package com.shoriext.blog.repository;
 
-public interface PostRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.shoriext.blog.model.Post;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+    // Найдем все посты, отсортированные по дате создания (от новых к старым)
+    List<Post> findAllByOrderByCreatedAtDesc();
+
+    // Найдем все посты конкретного автора
+    List<Post> findByAuthorOrderByCreatedAtDesc(String author);
 }
