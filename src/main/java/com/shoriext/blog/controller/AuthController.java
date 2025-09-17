fbe9eff24de90fd537b182b1abbd22781787668b
Samadar1 <urapol18@gmail.com>
2025-09-17 23:59:35 +0300

    add: get method /me

--- src/main/java/com/shoriext/blog/controller/AuthController.java
+++ src/main/java/com/shoriext/blog/controller/AuthController.java
@@ -4,15 +4,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.shoriext.blog.dto.SignUpDto;
+import com.shoriext.blog.model.User;
+import com.shoriext.blog.repository.UserRepository;
 import com.shoriext.blog.service.UserServiceImpl;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
+import java.net.Authenticator;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -20,12 +29,24 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class AuthController {
 
     private final UserServiceImpl userServiceImpl;
+    private final UserRepository userRepository;
 
     @PostMapping("/singup")
     public ResponseEntity<String> registerUser(@Valid @RequestBody SignUpDto signUpDto) {
         userServiceImpl.registerNewUser(signUpDto);
 
         return new ResponseEntity<>("User registered successfully!", HttpStatus.CREATED);
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<User> getCurrentUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+
+        return ResponseEntity.ok(user);
     }
 
 }
