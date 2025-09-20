83fb39ab2ba779ab5c119fd4b05052f22f41bdd7
Samadar1 <urapol18@gmail.com>
2025-09-20 17:09:03 +0300

    add: pagination method

--- src/main/java/com/shoriext/blog/repository/PostRepository.java
+++ src/main/java/com/shoriext/blog/repository/PostRepository.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import com.shoriext.blog.model.Post;
 import com.shoriext.blog.model.User;
@@ -16,4 +18,11 @@ public interface PostRepository extends JpaRepository<Post, Long> {
 
     // Найдем все посты конкретного автора
     List<Post> findByUserOrderByCreatedAtDesc(User user);
+
+    // Метод с пагинацией
+    Page<Post> findAll(Pageable pageable);
+
+    // Метод с пагинацией и сортировкой по дате (новые сначала)
+    Page<Post> findAllByOrderByCreatedAtDesc(Pageable pageable);
+
 }
